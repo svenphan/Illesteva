e9bd052abe2aaff1893d9ac461c434d82df33f91
tmphan <tmphan@sfu.ca>
2015-03-06 14:32:36 -0800

    modal fixes

--- js/productListingModal.js
+++ js/productListingModal.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.seeMoreBtn,#modalBG').hide();
+	$('.seeMoreBtn,#modalBG,#reviewSection,#clearGlasses').hide();
 	$('#leonards').on('click',function(){
 		$('#modalBG').show();
 		$('body').css({'overflow': 'hidden','height': '100%'});
@@ -25,12 +25,38 @@ $('.seeMoreBtn').on('click',function(){
 $('#comments').on('click',function(){
 	$('#leftContent').addClass('comments');
 	$('.closeBtn').addClass('comments')
+	$('.eyeGlassContainer').hide();
+	$('#reviewSection').show();
+
 });
 
 $('.closeBtn').on('click',function(){
 	$('#modalBG').hide();
 	$('body').css({'overflow': 'scroll','height': '100%'});
 	$('#leftContent,.closeBtn').removeClass('comments');
+	$('#reviewSection').hide();
+	$('.eyeGlassContainer').show();
 })
 
+$('#viewProduct').on('click',function(){
+	$('#leftContent').removeClass('comments');
+	$('#leftContent').addClass('productView');
+	$('.closeBtn').removeClass('comments')
+	$('.eyeGlassContainer').show();
+	$('#reviewSection').hide();
+});
+
+$('#grey').on('click',function(){
+	$('#grey').css({'border': '3px solid black'});
+	$('#blue').css({'border': '0px'});
+	$('#blueGlasses').hide();
+	$('#clearGlasses').fadeIn();
+});
+
+$('#blue').on('click',function(){
+	$('#blue').css({'border': '3px solid black'});
+	$('#grey').css({'border': '0px'});
+	$('#clearGlasses').hide();
+	$('#blueGlasses').fadeIn();
+});
 });
