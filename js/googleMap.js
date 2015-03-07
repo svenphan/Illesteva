21158a5520166cf0f262e24e41abb62ffc4a7cef
levonayim <levona10@hotmail.com>
2015-03-06 16:46:20 -0800

    put a marker for where the store location is and fixed the styling

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,9 +1,21 @@
-function initialize() {
-	  var mapProp = {
-		center:new google.maps.LatLng(40.723384,-74.001704),
-		zoom:15,
-		mapTypeId:google.maps.MapTypeId.ROADMAP
-	  };
-	  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
-	}
-	google.maps.event.addDomListener(window, 'load', initialize);
+/** With help from http://www.w3schools.com/googleapi/tryit.asp?filename=tryhtml_map_overlays_marker **/
+var myCenter=new google.maps.LatLng(40.72362,-73.99586);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:18,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
